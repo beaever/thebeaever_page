9e0328bc8f6cef88531e18c6242d6b002481092e
beaever <thebeaever@gmail.com>
2023-06-04 23:26:27 +0900

    기본적인 스타일 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,14 +14,23 @@ const textStyle = css`
   ${Fonts.style.body6}
 `;
 
+const contentStyle = css`
+  cursor: pointer;
+`;
+
 type DarkModeType = 'light' | 'dark';
 
 const Header = (): JSX.Element => {
   const [darkMode, setDarkMode] = useState<DarkModeType>('light');
 
+  const onClickChangeDarkMode = (mode: DarkModeType) => {
+    const changeMode = mode === 'dark' ? 'light' : 'dark';
+    setDarkMode(() => changeMode);
+  };
+
   return (
     <header css={containerStyle}>
-      <div>
+      <div css={contentStyle} onClick={() => onClickChangeDarkMode(darkMode)}>
         <span css={textStyle}>
           {darkMode === 'light' ? 'DARK MODE' : 'LIGHT MODE'}
         </span>
