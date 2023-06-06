97d8a527c154efd9839956efdf08009bfd1c4bb5
beaever <thebeaever@gmail.com>
2023-06-06 15:27:55 +0900

    Header 선언 위치 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from '@/styles/GlobalStyle';
 import type { AppProps } from 'next/app';
 import { theme } from '@/styles/themes';
-import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Header />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
