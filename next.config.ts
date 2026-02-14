c26cd142075f1914afdf6a7dcf169e183bbf4bfd
beaever <thebeaever@gmail.com>
2026-02-14 19:47:13 +0900

    [fix] basePath 설정 수정 - GitHub Pages 배포 시 스타일 적용 문제 해결

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
-  basePath: '',
+  basePath: '/portfolio',
   images: {
     unoptimized: true,
   },
